4b23e3f1b061ecaf7b1dfbb26895a301c736c624
Manas Najmuddeen <nmmanas@gmail.com>
2022-02-20 06:24:19 +0530

    fixed copy-paste issue

--- inventory/views.py
+++ inventory/views.py
@@ -4,9 +4,8 @@ from django.shortcuts import render
 
 import requests
 
-def call_drf_api_endpoint(api):
-
-	url = 'http://127.0.0.1:8000/' + api
+def call_drf_api_endpoint(request, api):
+	url = request.build_absolute_uri(api)
 	response = requests.get(url)
 	json_data = response.json()
 
@@ -14,15 +13,13 @@ def call_drf_api_endpoint(api):
 
 
 def inventory(request):
-	# url = request.build_absolute_uri('/api/inventory')
-	api = 'api/inventory/'
-	inventory_items = call_drf_api_endpoint(api)
+	api = '/api/inventory/'
+	inventory_items = call_drf_api_endpoint(request, api)
 
 	return render(request,'inventory/inventory.html', {'inventory_items': inventory_items})
 
 def inventory_detail(request, id):
-	# url = request.build_absolute_uri(f'/api/inventory/{id}')
-	api = f'api/inventory/{id}'
-	inventory_items = call_drf_api_endpoint(api)
+	api = f'/api/inventory/{id}'
+	inventory = call_drf_api_endpoint(request, api)
 
 	return render(request,'inventory/inventory_detail.html', {'inventory': inventory})
